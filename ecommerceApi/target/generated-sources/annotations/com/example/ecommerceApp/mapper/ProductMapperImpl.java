dfa3ccad8b80f28e404cdc740c5400804458e6b6
Salma-Abdallah <119084967+Salma-Abdallah@users.noreply.github.com>
2025-03-13 10:28:19 +0200

    updates

--- ecommerceApi/target/generated-sources/annotations/com/example/ecommerceApp/mapper/ProductMapperImpl.java
+++ ecommerceApi/target/generated-sources/annotations/com/example/ecommerceApp/mapper/ProductMapperImpl.java
@@ -7,8 +7,8 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2025-03-12T10:55:12+0200",
-    comments = "version: 1.5.3.Final, compiler: javac, environment: Java 17.0.5 (Oracle Corporation)"
+    date = "2025-03-13T09:53:34+0200",
+    comments = "version: 1.5.3.Final, compiler: Eclipse JDT (IDE) 3.41.0.z20250213-2037, environment: Java 21.0.6 (Eclipse Adoptium)"
 )
 @Component
 public class ProductMapperImpl implements ProductMapper {
@@ -21,11 +21,11 @@ public class ProductMapperImpl implements ProductMapper {
 
         Product.ProductBuilder product = Product.builder();
 
+        product.description( productDto.getDescription() );
         product.id( productDto.getId() );
         product.name( productDto.getName() );
         product.price( productDto.getPrice() );
         product.quantity( productDto.getQuantity() );
-        product.description( productDto.getDescription() );
 
         return product.build();
     }
@@ -38,11 +38,11 @@ public class ProductMapperImpl implements ProductMapper {
 
         ProductDto.ProductDtoBuilder productDto = ProductDto.builder();
 
+        productDto.description( product.getDescription() );
         productDto.id( product.getId() );
         productDto.name( product.getName() );
         productDto.price( product.getPrice() );
         productDto.quantity( product.getQuantity() );
-        productDto.description( product.getDescription() );
 
         return productDto.build();
     }
